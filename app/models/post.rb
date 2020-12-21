dda1594ef17ef00dd1ba07bf64daee99a21d2e20
MaryAnnN28 <maryann.navarrete@gmail.com>
2020-12-21 15:51:15 -0500

    commit tree 05646c0c73c099da597707fe3c4be925df79d67f

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,9 @@
 class Post < ActiveRecord::Base
+     validates :title, presence: true
+     validates :content, length: { minimum: 200 }
+     validates :summary, length: { maximum: 30 }
+     validates :category, :inclusion => { :in => %w(Fiction Non-Fiction) }
+     validates_with TitleValidator
+
+
 end
